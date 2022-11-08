c5d2c833e5296de55f76190338d5bbeabb8b485e
Nick Welles <nickwelles@live.nl>
2022-11-08 14:39:36 +0100

    removed tests that are not used (for now). redo CI file. redo test script

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {CategoriesComponent} from "./categories/categories.component";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {CategoriesService} from "./categories/categories.service";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        CategoriesComponent,
       ],
+      providers: [CategoriesService]
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
@@ -24,12 +31,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('instruweb-front-end');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('instruweb-front-end app is running!');
   });
 });
