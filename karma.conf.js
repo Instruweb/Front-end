4b1aa16e7a48e4763530752cce660d227d1b9f33
Nick Welles <nickwelles@live.nl>
2022-12-19 10:52:14 +0100

    add sonarqube configuration to karma

--- karma.conf.js
+++ karma.conf.js
@@ -40,6 +40,16 @@ module.exports = function (config) {
       reports: ['lcovonly'],
       fixWebpackSourcePaths: true
     },
+    sonarqubeReporter: {
+      basePath: 'src/app', // test files folder
+      filePattern: '**/*spec.ts', // test files glob pattern
+      encoding: 'utf-8', // test files encoding
+      outputFolder: 'reports', // report destination
+      legacyMode: false, // report for Sonarqube < 6.2 (disabled)
+      reportName: function (metadata) {
+        return 'sonarqube_report.xml';
+      },
+    },
     reporters: ['progress', 'kjhtml', 'sonarqube'],
     port: 9876,
     colors: true,
