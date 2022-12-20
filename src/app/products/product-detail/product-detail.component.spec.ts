9064c28b3a2bce73cbac9131f06765cbdb8207cf
Nick Welles <nickwelles@live.nl>
2022-12-20 15:09:32 +0100

    added e2e tests

--- src/app/products/product-detail/product-detail.component.spec.ts
+++ src/app/products/product-detail/product-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import {fakeAsync, TestBed} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {ProductDetailComponent} from "./product-detail.component";
