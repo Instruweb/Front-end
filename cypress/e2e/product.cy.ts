e031de3cab9f6c896a695fe974a5ebba8610f78c
Nick Welles <nickwelles@live.nl>
2022-12-22 11:46:42 +0100

    added stress/load test script

--- cypress/e2e/product.cy.ts
+++ cypress/e2e/product.cy.ts
@@ -1,9 +1,23 @@
 describe("get one product", () => {
-  beforeEach(() => {
+  it("Should load one product", () => {
     cy.visit("/products/detail/1");
+    cy.get('.product_title').should("have.text", "Fazley Outlaw Series Sheriff Basic HH Green elektrische gitaar met gigbag")
   });
 
-  it("Should load", () => {
+  it("Visit a product by navigating from the home page", () => {
+    Cypress.config('defaultCommandTimeout', 10000);
+    cy.visit('/');
+    cy.contains('Alle categorieën').click();
+    cy.window()
+      .its('performance')
+      .invoke('mark', 'pageOpen')
+
+    cy.contains("Gitaren").click();
+    cy.contains("More info").click();
     cy.get('.product_title').should("have.text", "Fazley Outlaw Series Sheriff Basic HH Green elektrische gitaar met gigbag")
+
+    cy.window()
+      .its('performance')
+      .invoke('measure', 'pageOpen')
   });
 });
